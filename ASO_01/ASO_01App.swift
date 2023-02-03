11c7ff9746631cc006563833f6c00561aee827da
Kevin <kevinserda.pro@gmail.com>
2023-02-03 02:38:58 +0100

    Initial Commit

---
+++ ASO_01/ASO_01App.swift
@@ -0,0 +1,17 @@
+//
+//  ASO_01App.swift
+//  ASO_01
+//
+//  Created by apprenant16 on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ASO_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
