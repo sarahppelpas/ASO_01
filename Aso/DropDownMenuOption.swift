f384ca64cb421d507c34d2d312f1fe8aec2307d4
Kevin118218 <123653058+Kevin118218@users.noreply.github.com>
2023-02-03 10:39:38 +0100

    Update DropDownMenuOption.swift

--- Aso/DropDownMenuOption.swift
+++ Aso/DropDownMenuOption.swift
@@ -14,23 +14,7 @@ struct DropdownMenuOption: Identifiable, Hashable {
     let option: String
 }
 
-extension DropdownMenuOption {
-    static let testSingleMonth: DropdownMenuOption = DropdownMenuOption(option: "March")
-    static let testAllMonths: [DropdownMenuOption] = [
-        DropdownMenuOption(option: "January"),
-        DropdownMenuOption(option: "February"),
-        DropdownMenuOption(option: "March"),
-        DropdownMenuOption(option: "April"),
-        DropdownMenuOption(option: "May"),
-        DropdownMenuOption(option: "June"),
-        DropdownMenuOption(option: "July"),
-        DropdownMenuOption(option: "August"),
-        DropdownMenuOption(option: "September"),
-        DropdownMenuOption(option: "October"),
-        DropdownMenuOption(option: "November"),
-        DropdownMenuOption(option: "December")
-    ]
-}
+
 
 
 struct Association: Identifiable, Hashable {
